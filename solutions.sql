b8b3dfb3bdbfc6417f49ee2afa683891dc2cc879
JosiahPhua <josiahphuajy@gmail.com>
2021-06-30 22:31:56 +0800

    added solutions

--- solutions.sql
+++ solutions.sql
@@ -1,1 +1,43 @@
 -- Enter solutions here --
+SELECT * 
+	FROM routes 
+	JOIN airports AS origin
+	ON routes.origin_id = origin.id  
+	JOIN airports AS dest
+	ON routes.dest_id = dest.id
+	WHERE origin.city = 'New York' 
+	AND dest.city = 'Paris';
+
+
+SELECT count(*) 
+	FROM routes 
+	JOIN airports AS origin
+	ON routes.origin_id = origin.id  
+	JOIN airports AS dest
+	ON routes.dest_id = dest.id
+	WHERE origin.city = 'New York' 
+	AND dest.city = 'Paris';
+
+
+SELECT DISTINCT ON (airline_code) *
+	FROM routes 
+	JOIN airports AS origin
+	ON routes.origin_id = origin.id  
+	JOIN airports AS dest
+	ON routes.dest_id = dest.id
+	JOIN airlines AS plane
+	ON routes.airline_id = plane.id
+	WHERE origin.city = 'New York' 
+	AND dest.city = 'Paris';
+
+
+SELECT dest.city, COUNT(dest.city)
+	FROM routes 
+	JOIN airports AS origin
+	ON routes.origin_id = origin.id  
+	JOIN airports AS dest
+	ON routes.dest_id = dest.id
+	JOIN airlines AS plane
+	ON routes.airline_id = plane.id
+	WHERE origin.city = 'New York' 
+	GROUP BY dest.city;
